f3154583b9074d038be10106189fc43b07945f2f
sidney!! <68286409+Sidnehh@users.noreply.github.com>
2026-02-07 06:02:54 +0100

    Fixed Title Screen

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -7,4 +7,4 @@ func _on_options_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Assets/Scenes/options.tscn")
 
 func _on_exit_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
